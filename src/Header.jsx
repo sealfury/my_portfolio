3458dce5b847629787c322eeafbf84adcf5d6942
William <willspecker@gmail.com>
2020-11-03 13:54:37 +0100

    Adding Semantic UI framework, adding some simple css, and implementing the css in previously defined js components for Header and Footer

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React from "react"
+import { Menu, Segment } from "semantic-ui-react";
 
 const Header = () => {
     return (
-        <nav>
-            <h1 id="header">My Portfolio</h1>
-        </nav>
+       <Segment inverted>
+           <Menu inverted secondary>
+               <Menu.Item
+                 name="my portfolio"
+               />
+           </Menu>
+       </Segment>
     );
 };
 
