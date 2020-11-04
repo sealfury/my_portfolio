a44fcb767bd972595c3c25f6519ddb7bf583a13b
William <willspecker@gmail.com>
2020-11-04 10:24:19 +0100

    Creating cypress tests to test functionality of navigation around the portfolio and the contents of the projects & about tabs

---
+++ src/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const About = () => {
+    return (
+        <Container>
+            <h1 id="about-header">About Me</h1>
+            <p>
+                I definitely just wasted a not inconsiderable amount of time looking 
+                up the background of a popular but apparently nonsensical latin phrase.
+            </p>
+        </Container>
+    );
+};
+
+export default About; 
