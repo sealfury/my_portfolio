02f925333e0167fb472f86d97c03126ba1a9976c
William <willspecker@gmail.com>
2020-11-04 13:36:15 +0100

    Refactoring Projects component to a stateful class, setting initial state for projects, modifying the render function with key attribute et al

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 
-const Projects = () => {
-    return (
-        <Container>
-            <h1 id="projects-header">My Projects</h1>
-            <p>
-                Let's maybe look into potential templates on semantic-UI for this part 
-                so the links to the github repo seem a bit more appealing?
-            </p>
-        </Container>
-    );
-};
+class Projects extends Component {
+    state = {
+        projects: [
+            {
+                id: 1,
+                name: "My First Website",
+            },
+            {
+                id: 2,
+                name: "FizzBuzz",
+            },
+        ],
+    };
 
-export default Projects
+    render() {
+        const { projects } = this.state;
+
+        let projectsList = projects.map((project) => {
+            return (
+                <div id={`project-${project.id}`} key={project.id}>
+                    <h3>{project.name}</h3>
+                </div>
+            );
+        });
+
+        return (
+            <Container>
+                <h1 id="projects-header">My Projects</h1>
+                {projectsList}
+            </Container>
+        );
+    }
+}
+
+export default Projects;
