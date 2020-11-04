8172d203188e383f4b56a0455765266b5e1c3962
William <willspecker@gmail.com>
2020-11-04 11:40:39 +0100

    Adding Switch and Route componens and implementing them in App component, adding Link and NavLink components and implementing them in Header component so that all tests pass

---
+++ src/Projects.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const Projects = () => {
+    return (
+        <Container>
+            <h1 id="projects-header">My Projects</h1>
+            <p>
+                Let's maybe look into potential templates on semantic-UI for this part 
+                so the links to the github repo seem a bit more appealing?
+            </p>
+        </Container>
+    );
+};
+
+export default Projects
