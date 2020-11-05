4afd117a289afaf7a3da414436f1ad5d5c4d2741
William <willspecker@gmail.com>
2020-11-05 11:47:38 +0100

    Removing webpack dependency

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "semantic-ui-css/semantic.min.css";
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <BrowserRouter>
