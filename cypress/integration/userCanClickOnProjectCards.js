9e94b49bee5190bebb09249c428be2f653b87a5b
William <willspecker@gmail.com>
2020-11-06 13:28:24 +0100

    Creating tests for functionality of clickable card links (not passing), adding content to Hello.jsx

---
+++ cypress/integration/userCanClickOnProjectCards.js
@@ -0,0 +1,17 @@
+
+describe("User can click on project cards", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+        cy.get("#projects-tab").click();
+    });
+
+    it("links to first project", () => {
+        cy.get("#project-1").click();
+        cy.get(".link").should("contain", "https://github.com/sealfury/address_book_challenge"); 
+    });
+
+    it("links to second project", () => {
+        cy.get("#project-2").click();
+        cy.get(".link").should("contain", "https://github.com/sealfury/fizzbuzz"); 
+    });
+}) 
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import { Link , NavLink } from "react-router-dom";
 
 const Hello = () => {
     return (
         <Container>
-            <h1 id="hello">Hello World</h1>
+            <h2 id="welcome">Welcome To My Portfolio</h2>
+            <p>My portfolio is empty at the moment, but head over to the</p><NavLink id="My Projects" to="/projects"></NavLink><p> 
+            page to see the projects that I've finished so far.
+            </p>
         </Container>
             
         
