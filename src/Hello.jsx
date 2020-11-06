9b80416d7898cae7342da8668c270c637ba22493
William <willspecker@gmail.com>
2020-11-06 13:44:58 +0100

    Editing Hello.jsx so that link in description navigates to My Projects page

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
-import { Link , NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Hello = () => {
     return (
         <Container>
+            <div>
             <h2 id="welcome">Welcome To My Portfolio</h2>
-            <p>My portfolio is empty at the moment, but head over to the</p><NavLink id="My Projects" to="/projects"></NavLink><p> 
-            page to see the projects that I've finished so far.
+            <p>My portfolio is empty at the moment, but head over to the<Link to="/projects">My Projects</Link>  
+              page to see the projects that I've finished so far.
             </p>
+            </div>
         </Container>
             
         
