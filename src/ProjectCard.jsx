f3283e0c85adfcba80aa25797f9ee67aa469982f
William <willspecker@gmail.com>
2020-11-08 16:40:38 +0100

    Removing Link from imported components on project card to attempt to fix compiling error

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Card, Image} from "semantic-ui-react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const ProjectCard = ({ project }) => {
     return (
